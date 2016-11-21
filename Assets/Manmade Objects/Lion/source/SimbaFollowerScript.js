2ff7856efa7250b090d4dc9352f4846a06d82001
wangleibeckham <wangleibeckham@gmail.com>
2016-11-21 14:02:09 -0500

    add lion

---
+++ Assets/Manmade Objects/Lion/source/SimbaFollowerScript.js
@@ -0,0 +1,22 @@
+﻿#pragma strict
+
+
+var anim: Animator;
+var posX:float;
+var posY:float;
+var posZ:float;
+//var speed=20f;
+var cameraRig: Transform;
+var originalLoc: Vector3;
+
+function Start () {
+	cameraRig = GameObject.Find("[CameraRig]").transform;
+	anim = GetComponent.<Animator>();
+	transform.LookAt(Vector3(cameraRig.position.x,transform.position.y,cameraRig.position.z));
+
+}
+
+function FixedUpdate () {
+	transform.LookAt(Vector3(cameraRig.position.x,transform.position.y,cameraRig.position.z));
+	transform.position = Vector3.MoveTowards(transform.position, GameObject.Find("[CameraRig]").transform.position, 0.05);
+}
