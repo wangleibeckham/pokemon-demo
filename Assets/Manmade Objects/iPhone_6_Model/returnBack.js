90bf9ad120e294799400cd86f6508d7265696fd1
wangleibeckham <wangleibeckham@gmail.com>
2016-12-05 02:51:02 -0500

    refactor code

--- Assets/Manmade Objects/iPhone_6_Model/returnBack.js
+++ Assets/Manmade Objects/iPhone_6_Model/returnBack.js
@@ -2,21 +2,23 @@
 
 var cameraRig: Transform;
 var secretLoc: Vector3;
+var performed: boolean;
 
 function Start () {
 	cameraRig = GameObject.Find("[CameraRig]").transform;
 	secretLoc = Vector3(3.94, 0, 4.93);
-
+	performed = false;
 }
 
 function Update () {
 //	Debug.Log(gameObject.transform.position.y);
 //	 if the iphone is dropped, use 995 as hardcoded location indicator, should use relative gameObject.transform.position location instead
-	if (gameObject.transform.position.y<995)
+	if (gameObject.transform.position.y<995 && !performed)
 	{
 		Debug.Log("Go Back!");
 		cameraRig.position = secretLoc;
 		cameraRig.rotation = Quaternion.AngleAxis(90, Vector3.up);
+		performed= true;
 	}
 
 }
--- Assets/Manmade Objects/iPhone_6_Model/transportTo.js
+++ Assets/Manmade Objects/iPhone_6_Model/transportTo.js
@@ -2,20 +2,22 @@
 
 var cameraRig: Transform;
 var secretLoc: Vector3;
+var performed: boolean;
 
 function Start () {
 	cameraRig = GameObject.Find("[CameraRig]").transform;
 	secretLoc = Vector3(-994, 998, 1001);
-
+	performed = false;
 }
 
 function Update () {
 	// if the iphone is dropped, use -2 as hardcoded location indicator, should use relative gameObject.transform.position location instead
-	if (gameObject.transform.position.y<-2)
+	if (gameObject.transform.position.y<-2 && !performed)
 	{
 		Debug.Log("Transport To Secret Location");
 		cameraRig.position = secretLoc;
 		cameraRig.rotation = Quaternion.AngleAxis(-90, Vector3.up);
+		performed= true;
 	}
 
 }
