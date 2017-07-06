9869a28f26340f8ddc5755f04cb41bb9f38c51fb
Zhenwei Wang <wangleibeckham@gmail.com>
2017-07-06 15:45:52 -0400

    update for sense of direction test

--- Assets/Manmade Objects/animated dog/dogScript.js
+++ Assets/Manmade Objects/animated dog/dogScript.js
@@ -1,6 +1,6 @@
 ﻿#pragma strict
 
-
+var anim: Animator;
 var posX:float;
 var posY:float;
 var posZ:float;
@@ -8,11 +8,40 @@ var cameraRig: Transform;
 
 function Start () {
 	cameraRig = GameObject.Find("[CameraRig]").transform;
+	anim = GetComponent.<Animator>();
+
 	transform.LookAt(Vector3(cameraRig.position.x,transform.position.y,cameraRig.position.z));
+
+	anim.SetBool("dog_walk",false);
+	anim.SetBool("happy",true);
 }
 
 function FixedUpdate(){
 	// use fixed update for lookat function, so the wait time is fixed, not subject to func running time.
-	transform.LookAt(Vector3(cameraRig.position.x,transform.position.y,cameraRig.position.z));
+
+	posX = GameObject.Find("[CameraRig]").transform.Find("Camera (eye)").transform.position.x;
+	posZ = GameObject.Find("[CameraRig]").transform.Find("Camera (eye)").transform.position.z;
+
+	if ((posX <= (transform.position.x)+1) && (posX >= (transform.position.x)-1))
+	{
+	if ((posZ <= (transform.position.z)+1) && (posZ >= (transform.position.z)-1))
+	{
+	anim.SetBool("dog_walk",true);
+	anim.SetBool("happy",false);
+	}
+
+
+	}else
+	{
+	 transform.LookAt(Vector3(cameraRig.position.x,transform.position.y,cameraRig.position.z));
+	 anim.SetBool("dog_walk",false);
+	 anim.SetBool("happy",true);
+	}
+
+
+
+
+
+
 
 }
